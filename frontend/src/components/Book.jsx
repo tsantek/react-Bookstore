c1f591aed5edf82f9de7f2ade57f066f45a23422
tsantek <santek.vg@gmail.com>
2019-07-09 11:06:44 -0700

    finish design carts

--- frontend/src/components/Book.jsx
+++ frontend/src/components/Book.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
 const Book = ({book}) =>{
-    console.log(book)
+
+
+    let date = new Date(book.published);
+    let convertedDate = `${date.getMonth() + 1}/${date.getFullYear()} `
+   
+
+
     return (
         <div className="col-sm-6 book-item-container">
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{book.title}</h5>
-                    <p className="card-text">{book.subtitle}</p>
+                    <p className="card-text"><b>{book.subtitle}</b></p>
                     <p className="card-text">{book.Author}</p>
                     <p className="card-text">{book.description}</p>
-                    <p className="card-text">{book.published} by {book.publisher}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text">Published: {convertedDate} by {book.publisher}</p>
+                    <p>Pages: ${book.pages}</p>
+                    <p>Price: <b>${book.price}</b></p>
+                    <a style={{float:"right"}} href="#" className="btn btn-primary">Add to Cart</a>
                 </div>
             </div>
         </div>
