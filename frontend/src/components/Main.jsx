965123a5b680771747e7c5fd559b2fe43f3f8878
tsantek <santek.vg@gmail.com>
2019-07-11 09:57:45 -0700

    update searchbar

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Books from './Books';
+import Books from "./Books";
 
 class Main extends Component {
-        state = { 
-            search : ""
-             }
+  state = {
+    search: "",
+    filderedBoos: []
+  };
 
-    handlSearchStateUpdateSearch = (input) => {
-        const str = input.target.value.toLowerCase();
-        this.setState(prevState =>{
-            return{
-                ...prevState,
-                search: str
-            }
-        })
-      }
-       
-    render() {
-        let filterd=[];
+  handlSearchStateUpdateSearch = input => {
+    const search = input.target.value.toLowerCase();
 
-        if(this.state.search.length > 0){
-            Object.keys(this.props.books).forEach(key => {
-                if(this.props.books[key].title.toLowerCase().includes(this.state.search) || this.props.books[key].author.toLowerCase().includes(this.state.search)){
-                   filterd.push(this.props.books[key])
-                }   
-            })
-        }else{
-            filterd = this.props.books;
-        }   
-       return (
-        <div>
-            <div className="input-group mb-3 search">
-                    <input type="text" className="form-control" placeholder="Search" onChange={e => this.handlSearchStateUpdateSearch(e)}/>
-             </div>
-                 <Books books={filterd} btn={this.state.btn} AddToCart={this.props.AddToCart}/>
-        </div>
-         )
+    let filderedBoos = [];
+
+    if (this.state.search.length > 0) {
+      Object.keys(this.props.books).forEach(key => {
+        if (
+          this.props.books[key].title
+            .toLowerCase()
+            .includes(this.state.search) ||
+          this.props.books[key].author.toLowerCase().includes(this.state.search)
+        ) {
+          filderedBoos.push(this.props.books[key]);
+        }
+      });
+    } else {
+      filderedBoos = this.props.books;
     }
+    this.setState({ search, filderedBoos });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="input-group mb-3 search">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            onChange={e => this.handlSearchStateUpdateSearch(e)}
+          />
+        </div>
+        <Books
+          books={this.state.search ? this.state.filderedBoos : this.props.books}
+          btn={this.state.btn}
+          AddToCart={this.props.AddToCart}
+        />
+      </div>
+    );
+  }
 }
- 
-export default Main ;
+
+export default Main;
