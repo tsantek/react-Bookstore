e7e93964178fedf81f6eda8e1e3b6a2af4abb45d
tsantek <santek.vg@gmail.com>
2019-07-13 07:36:47 -0700

    added env files

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8082/api',
-})
+  baseURL: `${process.env.REACT_APP_API_URL}/`
+});
